d89c1b23c7044b852489b9e062b7c976d35de532
TONIYA-DELL\Toniya <toniya.sundaram@gmail.com>
2018-04-06 16:55:37 +0800

    Migrating the contracts for deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,31 +1,18 @@
 var KeyValueStorage = artifacts.require("./KeyValueStorage.sol");
 var StoreNumber = artifacts.require("./StoreNumber.sol");
 var NumberLogicV1 = artifacts.require("./NumberLogicV1.sol");
-var NumberLogicV2 = artifacts.require("./NumberLogicV2.sol");
+
 
 module.exports = function(deployer) {
 
-  deployer.deploy(NumberLogicV1).then(function(){
-    console.log('NumberLogicV1 contract deployed successfully',NumberLogicV1.address)
-    deployer.deploy(NumberLogicV2).then(function(){
-        console.log('NumberLogicV2 contract deployed successfully',NumberLogicV2.address);
-        deployer.deploy(KeyValueStorage).then(function(){
-            console.log(KeyValueStorage.address);
-            deployer.deploy(StoreNumber,KeyValueStorage.address).then(function(){
-                StoreNumber.deployed().then(function(sn){
-                    console.log(sn.address);
-                    sn.upgradeTo(NumberLogicV1.address).then(function(res){
-                    console.log(res);
-                    })
-                }) 
-            });
+  deployer.deploy(NumberLogicV1);
+  deployer.deploy(KeyValueStorage);
+  deployer.deploy(StoreNumber,KeyValueStorage.address).then(function(){
+    StoreNumber.deployed().then(function(sn){
+        console.log(sn.address);
+        sn.upgradeTo(NumberLogicV1.address).then(function(res){
+        console.log(res);
         })
-    });
-  })
+    }) 
+});
 };
-
-
-
-
-
-
