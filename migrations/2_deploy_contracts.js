cb8645cf358c27d9d99003d5e0a6e5419bb308d1
TONIYA-DELL\Toniya <toniya.sundaram@gmail.com>
2018-04-08 11:34:36 +0800

    Deploying the contract one by one

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,13 +6,13 @@ var NumberLogicV1 = artifacts.require("./NumberLogicV1.sol");
 module.exports = function(deployer) {
 
   deployer.deploy(NumberLogicV1);
-  deployer.deploy(KeyValueStorage);
-  deployer.deploy(StoreNumber,KeyValueStorage.address).then(function(){
-    StoreNumber.deployed().then(function(sn){
-        console.log(sn.address);
-        sn.upgradeTo(NumberLogicV1.address).then(function(res){
-        console.log(res);
-        })
-    }) 
-});
+//   deployer.deploy(KeyValueStorage);
+//   deployer.deploy(StoreNumber,KeyValueStorage.address).then(function(){
+//     StoreNumber.deployed().then(function(sn){
+//         console.log(sn.address);
+//         sn.upgradeTo(NumberLogicV1.address).then(function(res){
+//         console.log(res);
+//         })
+//     }) 
+// });
 };
---
+++ test/StoreNumberTest.js
@@ -0,0 +1,4 @@
+import _ from 'lodash'
+import { expect } from 'chai'
+import { web3 } from './helpers/w3'
+import expectRevert from './helpers/expectRevert'
