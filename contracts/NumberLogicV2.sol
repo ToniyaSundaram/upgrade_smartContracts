2a51aefb2427e3b269a0ab3053aab059af1c4565
TONIYA-DELL\Toniya <toniya.sundaram@gmail.com>
2018-04-09 09:23:21 +0800

    Updating the Test File

--- contracts/NumberLogicV2.sol
+++ contracts/NumberLogicV2.sol
@@ -5,8 +5,8 @@ import "./StorageStateful.sol";
 contract NumberLogicV2 is StorageStateful{
     uint number;
     function setNumber(uint _number) {
-        _number = 20*_number;
-        _storage.setUint("MyNumber", _number);        
+        number = 20*_number;
+        _storage.setUint("MyNumber", number);        
     }
     
     function getNumber()public returns(uint){
